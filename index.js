8244ce49a3215fecc7b8e0de2391934f3fb4f1a6
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-08-19 13:42:47 -0500

    commit tree 605d4f9c94dae409aa74ba46fe556991e861971c

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+const num1 = 31
+const num2 = 2
+
+const multiply = num1 * num2
+console.log(multiply)
+
+let random = Math.ceil(Math.random() * 10) + 1;
+
+const num3 = 8
+const num4 = 2
+
+console.log(random)
+
+let mod = (num3 / num4)
+
+console.log(mod)
+
+const max = Math.max(1, 4, 5, 20)
